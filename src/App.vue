6f751e4f3d346b2339338d0cfe7e84d6578dbe52
Alexandre Geissmann <alex@la-bonaventure.com>
2020-12-18 15:39:43 -0500

    add back button

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
   <li><RouterLink :to="{ name: 'organization-index' }">Go to Index</RouterLink>
 </li>
   <li>  <RouterLink :to="{ name: 'organization-settings', params: {id: '0'} }">Go to details</RouterLink></li>
+  <li><button @click="$router.go(-1)">Back</button></li>
 </ul>
 </nav>
 </template>
